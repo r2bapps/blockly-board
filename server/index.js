eecb92544bdbb30ce5e06eca5bad6480144b34f8
R2B Apps <r2b.apps@gmail.com>
2019-11-15 17:48:42 +0100

    integrate startup with heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const http = require('http')
 const app = express();
 app.use(express.static('public'));
 const server = http.createServer(app);
-server.listen(port = 8080, hostname = 'localhost', () => {
-  console.debug(`Server running at http://${hostname}:${port}/`)
+server.listen(port = process.env.PORT || 8080, () => {
+  console.log(`Server running on port ${port}`)
 })
